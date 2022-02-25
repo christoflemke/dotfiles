f89cdf390287243d00989f725add7dd1e8c4b1ac
Christof Lemke <lchristo@microsoft.com>
2022-02-25 15:15:53 +0000

    Nice linkin of dotfiles

--- script/setup.sh
+++ script/setup.sh
@@ -6,10 +6,15 @@ DOTFILES_DIR=$(cd $(dirname "$0")/.. && pwd)
 echo "apt-install"
 $DOTFILES_DIR/script/apt-install.sh
 
+sync-folder() {
+    local source_dir=${1?'missing source_dir'}
+    local target_dir=${2?'missing target_dir'}
+    mkdir -p "$target_dir"
+    for dotfile in ${source_dir}/.*; do
+        target="${target_dir}/$(basename $dotfile)"
+        [ -L "$target" ] && rm "$target"
+        [ -f "$dotfile" ] && ln -s "$dotfile" "$target"
+    done    
+}
 
-echo "linking files"
-for dotfile in ${DOTFILES_DIR}/.*; do
-    target="${HOME}/$(basename $dotfile)"
-    [ -L "$target" ] && rm "$target"
-    [ -f "$dotfile" ] && ln -s "$dotfile" "$target"
-done 
+sync-folder "$DOTFILES_DIR"/homefs ~/
