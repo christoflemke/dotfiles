5035f9af2e72b6d83452d01f02833a41be9bfba9
Christof Lemke <christoflemke@github.com>
2022-03-25 13:15:16 +0100

    fix linking in codespaces

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 DOTFILES_DIR=$(cd $(dirname "$0") && pwd)
 
 echo "apt-install"
@@ -15,20 +13,25 @@ sync-folder() {
   local source_dir=${1?'missing source_dir'}
   local target_dir=${2?'missing target_dir'}
   mkdir -p "$target_dir"
-  for dotfile in $(find "$source_dir"); do
-    target="${dotfile/$source_dir/$target_dir}"
-    if [ -L "$target" ] || [ "$FORCE" == true ]; then
-      debug "Delete: $target"
-      rm -f "$target"
-    fi
-    if [ ! -e "$dotfile" ]; then
-      debug "linking $dotfile -> $target"
-      ln -s "$dotfile" "$target"
-    elif [ -d "$target" ]; then
-      debug "Skip directory $target"
-    else
-      echo "Skipping ${target}" >&2
-    fi
+
+  find "$source_dir" -type f | while read dotfile; do
+      target="${dotfile/$source_dir/$target_dir}"
+      if [ -L "$target" ]; then
+          debug "Delete Link: $target"
+          rm -f "$target"
+      fi
+      if [ -f "$target" ] && [ "$FORCE" == true ]; then
+          debug "Delete File: $target"
+          rm -f "$target"
+      fi
+
+      mkdir -p "$(dirname $target)"
+      if [ ! -e "$target" ]; then
+          debug "linking $dotfile -> $target"
+          ln -s "$dotfile" "$target"
+      else
+          echo "Skipping ${target}" >&2
+      fi
   done
 }
 
